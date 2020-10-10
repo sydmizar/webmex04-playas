e31ef01f7b14fbce62616f0de4ce6106a974f78d
sydmizar <irenebenigna@gmail.com>
2020-10-10 09:32:57 -0500

    Updates

---
+++ precache-manifest.6942281dc859a664aa9efbdb194e3ada.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c2da5af3e3b4d939f2614a3c862a5e",
+    "url": "/mex04-playas/index.html"
+  },
+  {
+    "revision": "cca2d21da8d26168705e",
+    "url": "/mex04-playas/static/css/main.8b94feef.chunk.css"
+  },
+  {
+    "revision": "c82002adf01192861cd2",
+    "url": "/mex04-playas/static/js/3.adec64d4.chunk.js"
+  },
+  {
+    "revision": "fc46b884f3fcccdc4cc01450637434c4",
+    "url": "/mex04-playas/static/js/3.adec64d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8acff4db6f141afbe15",
+    "url": "/mex04-playas/static/js/4.6ad60e05.chunk.js"
+  },
+  {
+    "revision": "796abc9903df64c603e6",
+    "url": "/mex04-playas/static/js/5.4451764b.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/mex04-playas/static/js/5.4451764b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac800081d63bc0b3960c",
+    "url": "/mex04-playas/static/js/6.833335d1.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/mex04-playas/static/js/6.833335d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca2d21da8d26168705e",
+    "url": "/mex04-playas/static/js/main.238c3b0c.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/mex04-playas/static/js/main.238c3b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa16ab3070b18a50249",
+    "url": "/mex04-playas/static/js/runtime-main.2ae316c5.js"
+  },
+  {
+    "revision": "ebe87df3161102ea1c26",
+    "url": "/mex04-playas/static/js/xlsx.81a7b92e.chunk.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/mex04-playas/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/mex04-playas/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/mex04-playas/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/mex04-playas/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/mex04-playas/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/mex04-playas/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/mex04-playas/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/mex04-playas/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/mex04-playas/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/mex04-playas/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/mex04-playas/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/mex04-playas/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/mex04-playas/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/mex04-playas/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/mex04-playas/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/mex04-playas/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/mex04-playas/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/mex04-playas/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "b14d82b1698a14f3d1f146c889f94c8b",
+    "url": "/mex04-playas/static/media/playa.b14d82b1.jpg"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/mex04-playas/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/mex04-playas/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/mex04-playas/static/media/uriel-soberanes.fe3a7838.jpg"
+  }
+]);
---
+++ static/js/xlsx.81a7b92e.chunk.js
@@ -0,0 +1,18 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+* Copyright 2020 Creative Tim (http://www.creative-tim.com)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+(this["webpackJsonppaper-kit-react"]=this["webpackJsonppaper-kit-react"]||[]).push([[2],{390:function(p,t){},478:function(p,t){},479:function(p,t){}}]);
+//# sourceMappingURL=xlsx.81a7b92e.chunk.js.map
